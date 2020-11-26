50d3bd35003f12f599f6b12940b8980410167927
Kristina Egorova <kristinaeo2001@mail.ru>
2020-11-26 15:10:31 +0300

    lesson 2

---
+++ Source.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+int main()
+/*{
+	float v = 0;
+	float r = 0;
+	float h = 0;
+	printf("vvedite radius i visotu");
+	scanf_s("%f", &r);
+	scanf_s("%f", &h);
+	v = r * r * h * 3.14;
+	printf("obem figuri: %f\n", v);
+	return 0;
+}*/
+
+/*{
+	float s = 0;
+	float r = 0;
+	float h = 0;
+	printf("vvedite radius i visotu");
+	scanf_s("%f", &r);
+	scanf_s("%f", &h);
+	s = (2 * 3.14 * r * r) + (2 * 3.14 * r * h);
+	printf("obem figuri: %f\n", s);
+	return 0;
+}*/
+
+/*{
+	int time = 0;
+	int time2 = 0;
+	printf("vvedite vremya v minutah");
+	scanf_s("%i", &time);
+	time2 = time / 60; time = time % 60;
+	printf("%i\n", time2);
+	printf("%i\n", time);
+	return 0;
+}*/
+
+/*{
+	float a = 0;
+	float b = 0;
+	printf("vvedite ves");
+	scanf_s("%f", &a);
+	b = a*0.4059;
+	printf("%f\n", b);
+	return 0;
+}*/
+
+/*{
+	float a = 0;
+	float b = 0;
+	printf("vvedite ves");
+	scanf_s("%f", &a);
+	b = a * 1.0668;
+	printf("%f\n", b);
+	return 0;
+}*/
+
+/*{
+	float y = 0;
+	float u = 0;
+	float r = 0;
+	printf("vvedite radius i visotu");
+	scanf_s("%f", &u);
+	scanf_s("%f", &r);
+	y = u/r;
+	printf(" %f\n", y);
+	return 0;
+}*/
+
+/*{
+	float a = 0;
+	float b = 0;
+	printf("vvedite ves");
+	scanf_s("%f", &a);
+	b = a * 10;
+	printf("%f\n", b);
+	return 0;
+}*/
+
+/*{
+	float y = 0;
+	float u = 0;
+	float r = 0;
+	printf("vvedite radius i visotu");
+	scanf_s("%f", &u);
+	scanf_s("%f", &r);
+	y = (u * r)/(u+r);
+	printf(" %f\n", y);
+	return 0;
+}*/
+
+/*{
+	float y = 0;
+	float u = 0;
+	float r = 0;
+	printf("vvedite radius i visotu");
+	scanf_s("%f", &u);
+	scanf_s("%f", &r);
+	y = u * r;
+	printf(" %f\n", y);
+	return 0;
+}*/
+
+/*{
+float y = 0;
+float u = 0;
+float r = 0;
+printf("vvedite radius i visotu");
+scanf_s("%f", &u);
+scanf_s("%f", &r);
+y = u*r;
+printf(" %f\n", y);
+return 0;
+}*/
+
+/*{
+float k = 0;
+float p = 0;
+float r = 0;
+float s = 0;
+scanf_s("%f", &r);
+scanf_s("%f", &p);
+scanf_s("%f", &k);
+s = 2*(k/100)*p*r;
+printf(" %f\n", s);
+return 0;
+}*/
+
+{
+	float k = 0;
+	float p = 0;
+	float r = 0;
+	float s = 0;
+	scanf_s("%f", &r);
+	scanf_s("%f", &p);
+	scanf_s("%f", &k);
+	s = k*p*r;
+	printf(" %f\n", s);
+	return 0;
+}
+
